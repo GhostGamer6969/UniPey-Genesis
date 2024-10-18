268a2a3d7b63e82a121e90638ed69093db56ebdd
GhostGamer6969 <arambhmani@hotmail.com>
2024-10-18 13:13:54 +0530

    Spark 2

--- routers/login.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const loginController = require('../controllers/login.controller');
-
-const loginRouter = express.Router();
-
-loginRouter.post('/send-login-otp', loginController.sendLoginOtp);
-loginRouter.post('/verify-login-otp', loginController.verifyLoginOtp);
-
-module.exports = loginRouter;
