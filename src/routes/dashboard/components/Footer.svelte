fa06badddc4fd1a70d379e96bae2fa1cd077adb8
GhostGamer6969 <arambhmani@hotmail.com>
2024-11-11 22:56:47 +0530

    fix

--- src/routes/dashboard/components/Footer.svelte
+++ src/routes/dashboard/components/Footer.svelte
@@ -15,23 +15,14 @@ can be found.
     <div class="grid-flow-col items-center">
         <ul class="menu menu-vertical md:menu-horizontal">
             <li>
-                <a href="https://www.stellar.org/terms-of-service" target="_blank"
-                    >Terms of Service</a
-                >
+                <a href="#" target="_blank">Terms of Service</a>
             </li>
             <li>
-                <a href="https://www.stellar.org/privacy-policy" target="_blank">Privacy Policy</a>
+                <a href="#" target="_blank">Privacy Policy</a>
             </li>
         </ul>
     </div>
     <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <ul class="menu">
-            <li>
-                <a href="https://github.com/GhostGamer6969/Celeriz" target="_blank">
-                    <GithubIcon size="16" />
-                    GitHub
-                </a>
-            </li>
-        </ul>
+        <ul class="menu"></ul>
     </div>
 </footer>
